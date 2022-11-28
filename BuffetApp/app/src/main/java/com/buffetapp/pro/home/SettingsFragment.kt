a3b60c73fce97e6b6a5b40d60ccc475423ea353a
Mario Roberto <mrg@treming.com>
2022-11-28 16:14:45 -0600

    add performance buttom and icon_buffet

--- BuffetApp/app/src/main/java/com/buffetapp/pro/home/SettingsFragment.kt
+++ BuffetApp/app/src/main/java/com/buffetapp/pro/home/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.buffetapp.pro.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.buffetapp.pro.OurServices.BodasActivity
 import com.buffetapp.pro.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -57,4 +60,5 @@ class SettingsFragment : Fragment() {
                 }
             }*/
     }
+
 }
